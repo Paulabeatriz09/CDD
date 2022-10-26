bbc7258c6a2e9d8fd27691b305a43719da27de39
Paula Beatriz <107193691+Paulabeatriz09@users.noreply.github.com>
2022-10-26 16:16:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from biblioteca import Conta
 
 c1=Conta(123,"Paula","Poupança")
 c1.ativar()
-print(c1.saldo)
+# print(c1.saldo)
 c1.depositar(100)
-print(c1.saldo)
-c1.sacar(10)
-c1.verificar_saldo()
+# print(c1.saldo)
+c1.ativar1(200)
+c1.nov()
+# c1.verificar_saldo()
+# c1.sacar(150)
+# ativa
+# deposita
+# saca

d5d52db5eb666b01efa53880a370339686b19fb7
Paula Beatriz <107193691+Paulabeatriz09@users.noreply.github.com>
2022-10-28 15:14:37 -0300

    Update biblioteca.py

---
+++ formas/biblioteca.py
@@ -0,0 +1,14 @@
+
+
+class Formas():
+    def __init__(self,area,perimetro):
+        self.area=area
+        self.perimetro=perimetro
+
+
+class Triangulo():
+    def _init_(self,altura):
+        super().__init__(area,perimetro)
+
+    def calculaPerimetro(self):
+        print()
